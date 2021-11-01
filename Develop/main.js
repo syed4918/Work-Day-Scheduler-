655eaa4dc1232815c26490b5b1df75b5af2dde43
Syed Jamil <syed.jamil13@gmail.com>
2021-11-01 19:10:08 -0400

    Tasks now render on page reload and further timeblocks added

--- Develop/main.js
+++ Develop/main.js
@@ -16,33 +16,58 @@ $(".saveBtn").click(function () {
     // get respective text and show in console
     var setValue = $(this).siblings(".form-control").val();
     console.log(setValue);
+var getTime = $(this).siblings(".form-control").attr("id");
+    console.log(getTime);
+    localStorage.setItem(getTime,setValue)
 
     // local storage function (happens when save button is clicked)
 
     // store task in local storage and convert JS into a string for EACH timeblock
-    for (var i = 0; i < 8 ; i++) {
-    var savedTask = localStorage.setItem(i, setValue);
     
-    }
 
 })
 
 
 // AND the information persists on page reload 
-function renderTask() {
+function renderTasks() {
 
-    // get local storage updated with tasks and convert text into JS object
-    var renderTask = JSON.parse(localStorage.getItem("1"));
-    $("").val(renderTask);
+// retrieve data from local storage  and convert text into JS object for each timeblock
+var render9AM = localStorage.getItem("9AM"); 
+$("#9AM").val(render9AM);
 
-    if (renderTask !== null) {
-        // select each individual timeblock and       
-    } else {
-        return;
-    }
+var render10AM = localStorage.getItem("10AM"); 
+$("#10AM").val(render10AM);
+
+var render11AM = localStorage.getItem("11AM"); 
+$("#11AM").val(render11AM);
+
+var render12PM = localStorage.getItem("12PM"); 
+$("#12PM").val(render12PM);
+
+var render1PM = localStorage.getItem("1PM"); 
+$("#1PM").val(render1PM);
+
+var render2PM = localStorage.getItem("2PM"); 
+$("#2PM").val(render2PM);
+
+var render3PM = localStorage.getItem("3PM"); 
+$("#3PM").val(render3PM);
+
+var render4PM = localStorage.getItem("4PM"); 
+$("#4PM").val(render4PM);
+
+var render5PM = localStorage.getItem("5PM"); 
+$("#5PM").val(render5PM);
+    
+
+    // if (renderTask !== null) {
+          
+    // } else {
+    //     return;
+    // }
 }
 
-renderTask();
+renderTasks();
 
 
 
